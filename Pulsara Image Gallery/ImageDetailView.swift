b663f84ef44a5144ace60882addcf38c5f49b03e
Taylor Carr <tayloracarr94@gmail.com>
2024-09-12 21:39:08 -0500

    Added image detail view

---
+++ Pulsara Image Gallery/ImageDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageDetailView.swift
+//  Pulsara Image Gallery
+//
+//  Created by Taylor Carr on 9/12/24.
+//
+
+import SwiftUI
+
+struct ImageDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageDetailView()
+}
